e6f727c461d9a1e49ece6ce42826420bbde1739b
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-01-22 21:55:41 -0300

    Add files via upload
    Activity 4 - sequential structure

---
+++ atividade4.py
@@ -0,0 +1,10 @@
+#Create a program that breaks down the 4 bimonthly grades and shows the average.
+
+grade1= float(input("Report your grade 1: "))
+grade2= float(input("Report your grade 2: "))
+grade3= float(input("Report your grade 3: "))
+grade4= float(input("Report your grade 4: "))
+
+avarege = (grade1 + grade2 + grade3 +grade4)/4
+
+print(f"your avarege is: {avarege}")
