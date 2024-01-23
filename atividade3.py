ef469450a4b204fadcdaa5557c11dfc10b378eab
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-01-22 21:52:03 -0300

    Add files via upload
    Atividade 3 - Estrutura Sequencial

---
+++ atividade3.py
@@ -0,0 +1,6 @@
+#Write a Program that asks for two numbers and prints the sum
+
+x = int(input("Enter a number: "))
+y = int(input("Enther another number: "))
+sum = x + y
+print(f"the result of the sum of  the {x} + {y} results in : {sum}" )
