470ad1afe72538f5de2d1a1f4212cd7707443a55
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-01-22 21:51:09 -0300

    Update atividade2.py

--- atividade2.py
+++ atividade2.py
@@ -1,4 +1,5 @@
 #Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+#Write a Program that asks for a number and then displays the message The number entered was [number].
 
 number = int(input("enter a number: "))
 print(f"The number given was: {number}")
