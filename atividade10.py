1dc24c2d7ce7ff1ccfe7209f08ae01bbd5229c9f
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-02-10 23:39:17 -0300

    Create atividade10.py

---
+++ atividade10.py
@@ -0,0 +1,8 @@
+# Pedindo ao usuário para inserir a temperatura em Celsius
+celsius = float(input("Digite a temperatura em graus Celsius: "))
+
+# Convertendo Celsius para Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Mostrando o resultado
+print(f"A temperatura em Fahrenheit é: {fahrenheit}°F")
