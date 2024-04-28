284c554aadfa239e105c968be05c77e4f9b578ce
WILLIAN DOUGLAS <67987291+Williands2003@users.noreply.github.com>
2024-04-27 22:11:27 -0300

    Update atividade6.py

--- atividade6.py
+++ atividade6.py
@@ -7,4 +7,4 @@ import math
 radius = float(input("Enter the radius of the circle: "))
 area = math.pi * radius ** 2
 
-print("The area of the circle is", round(area, 2))
+print("The area of the circle is", round( area, 2))
